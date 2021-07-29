993e32a3b9a90759d210ecb43c07b60e5cc96f3c
sujiaying@xiaomi.com <sujiaying@xiaomi.com>
2021-07-29 13:09:37 +0800

    main

--- src/test/Main.java
+++ src/test/Main.java
@@ -7,18 +7,19 @@ import test.tree.TreeNode;
 
 public class Main {
     public static void main(String[] args) {
-//        int[] date = {1,2,4,'#','#',5,'#',6,'#','#',3,7,9,'#','#','#',8,'#','#'};
-//        Tree tree = new Tree();
-//        TreeNode root = tree.createTree(date);
-//        System.out.println("root:"+root.val);
-//        tree.preStackOder(root);
+        //int[] date = {1,2,4,'#','#',5,'#',6,'#','#',3,7,9,'#','#','#',8,'#','#'};
+        int[] date = {1,2,'#','#',3,4,'#','#',5,'#','#'};
+        Tree tree = new Tree();
+        TreeNode root = tree.createTree(date);
+        System.out.println("root:"+root.val);
 
-        int[] date = {1,2,3,4,5};
-        Link link1 = new Link();
-        LinkNode head1 = link1.createTailLink(date);
-        link1.printLink(head1);
-        LinkNode headR=link1.reverseLink(head1);
-        link1.printLink(headR);
+
+//        int[] date = {1,2,3,4,5};
+//        Link link1 = new Link();
+//        LinkNode head1 = link1.createTailLink(date);
+//        link1.printLink(head1);
+//        LinkNode headR=link1.reverseLink(head1);
+//        link1.printLink(headR);
 
     }
 }
